917a5cc5799ca384d011a8ad0345a644c2178cb5
Devesh Raghuwanshi <ildevesh.cu@gmail.com>
2025-05-18 17:58:28 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-4xl font-bold text-purple-600">Tailwind 4 is working!</h1>
+      <p className="text-gray-700 mt-2">You're ready to move forward. 🚀</p>
+    </div>
+  );
+}
+
+export default App;
